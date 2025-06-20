090c0fdfb5b87c6be3c6b76ca568973fae31ba57
Aaliyah Symlar <aaliyahsymlar@Aaliyahs-MacBook-Air.attlocal.net>
2025-06-19 19:53:22 -0500

    implement companions homepage and companion builder form

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,51 @@
 import React from 'react'
 import {Button} from "@/components/ui/button";
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import Cta from "@/components/CTA";
+import {recentSessions} from "@/constants";
 
 const Page = () => {
   return (
-      <div>
-          <h1 className="text-2xl underline">Welcome to my SaaS App</h1>
-          <Button>Lets get started</Button>
-      </div>
+      <main>
+          <h1>Popular Companions</h1>
+
+          <section className="home-section">
+              <CompanionCard
+                  id="123"
+                  name="Neura the Brainy Explorer"
+                  topic="Neural Network of the Brain"
+                  subject="Math"
+                  duration={45}
+                  color="#ffda6e"
+              />
+              <CompanionCard
+                  id="456"
+                  name="Countsy the Number Wizard"
+                  topic="Derivatives & Integrals"
+                  subject="Science"
+                  duration={30}
+                  color="#e5d0ff"
+              />
+              <CompanionCard
+                  id="789"
+                  name="Verba the Vocabulary Builder"
+                  topic="English Literature"
+                  subject="Language"
+                  duration={30}
+                  color="#bde7ff"
+              />
+          </section>
+
+          <section className="home-section">
+              <CompanionsList
+                  title="Recently completed sessions"
+                  companions={recentSessions}
+                  classNames="w-2/3 max-lg:w-full"
+              />
+              <Cta />
+          </section>
+      </main>
 
   )
 }
